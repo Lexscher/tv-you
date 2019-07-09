75366b438ab28a9ecc2866198174c3a535ddd2e2
Alexander Schelchere <ascellie@gmail.com>
2019-07-09 16:50:58 -0400

    app(basic): tvshows [views, controller]

--- app/controllers/tvshows_controller.rb
+++ app/controllers/tvshows_controller.rb
@@ -1,2 +1,18 @@
 class TvshowsController < ApplicationController
+    before_action :find_tvshow, only: [:show]
+    
+
+    def index
+        @tvshows = Tvshow.all
+    end
+
+    def show
+    end
+
+    private
+
+    def find_tvshow
+        @tvshow = Tvshow.find(params[:id])
+    end
+
 end
