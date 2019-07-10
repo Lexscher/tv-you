cd6f83ff6c4859d814fd4c45c51bc17db18b1f84
Alexander Schelchere <ascellie@gmail.com>
2019-07-09 20:17:51 -0400

    db(seed): update

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,12 @@
 
 jack = User.create(name: "Jack Bean", username: "jack101")
 jess = User.create(name: "Jessica Bean", username: "jess101")
+mike = User.create(name: "Mike Wazowski", username: "mike101")
 
-fake_show = Tvshow.create(name: "clickbait", overview: "YOU'VE NEVER SEEN THIS BEFORE!!")
+fake_show1 = Tvshow.create(name: "clickbait", overview: "YOU'VE NEVER SEEN THIS BEFORE!!")
+fake_show2 = Tvshow.create(name: "The Vlogs", overview: "each episode is 4 minutes and 20 seconds long.")
+fake_show3 = Tvshow.create(name: "Moneyzzz", overview: "Learn how to get rich quick!")
+fake_show4 = Tvshow.create(name: "Chop Town", overview: "Learn how to chop 'em up and cook 'em down!")
 
 fav1 = Favorite.create(user_id: 1, tvshow_id: 1)
+fav2 = Favorite.create(user_id: 1, tvshow_id: 2)
