db3b456665efc16ad18ad3588e2270846bf89072
Francis Mendez <fmendez884@gmail.com>
2019-07-10 13:36:19 -0400

    create user views and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,55 @@
 class UsersController < ApplicationController
+
+  def index
+    all_users
+  end 
+
+  def new
+    @user = User.new
+  end 
+
+  def create
+    @user = User.create(user_params)
+
+    redirect_to @user
+
+  end 
+
+  def show
+    find_user
+  end 
+  
+  def edit 
+   find_user
+  end 
+
+  def update
+    find_user
+    #byebug
+    @user = @user.update(user_params)
+
+    redirect_to user_path
+  end 
+
+  def destroy
+    find_user
+    @user.destroy
+
+    redirect_to users_path
+  end 
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end 
+
+  def user_params
+    params.require(:user).permit(:name, :username)
+  end 
+
+  def all_users
+    @users = User.all 
+  end 
+
 end
