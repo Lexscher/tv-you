20379ba85949bdc9b8a449e5f85b8603d3397bef
Francis Mendez <fmendez884@gmail.com>
2019-07-11 11:36:11 -0400

    fixes auth and sessions functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  before_action :setup_auth
+
+  def setup_auth
+    @user_id = session[:user_id]
+    @logged_in = !!@user_id
+    if @logged_in
+      @current_user = User.find(@user_id)
+      
+    end
+  end
+
+  def authorized
+    #byebug
+    unless @logged_in
+      flash[:message] = "Nice try, you need to log in."
+      return redirect_to login_path
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    
+    has_secure_password
     has_many :favorites, dependent: :destroy
     has_many :tvshows, through: :favorites
 
